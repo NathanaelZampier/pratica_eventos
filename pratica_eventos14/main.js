c73e201d2c88a9010075ee5b7aa52e1f0cc4d58a
pier-ex <nathanaelrog@hotmail.com>
2026-04-26 10:00:49 -0300

    feat: implementação completa de validação de formulário com controle de estado

--- pratica_eventos14/main.js
+++ pratica_eventos14/main.js
@@ -8,24 +8,37 @@ const feedbackSenha = document.getElementById('feedback-senha')
 const feedbackGeral = document.getElementById('feedback-geral')
 const botao = document.getElementById('btn')
 
+botao.disabled = true
+
+function atualizarBotao() {
+    if (validarNome() && validarEmail() && validarSenha()) {
+        botao.disabled = false
+    } else {
+        botao.disabled = true
+    }
+}
+
 function validarNome() {
     const valorNome = campoNome.value.trim()
 
     if (!valorNome) {
         feedbackNome.textContent = 'Insira um nome'
         feedbackNome.style.color = 'red'
+        campoNome.style.borderColor = 'red'
         return false
     }
     
     else if (valorNome.length < 3) {
         feedbackNome.textContent = 'nome inválido'
         feedbackNome.style.color = 'red'
+        campoNome.style.borderColor = 'red'
         return false
     }
 
     else {
         feedbackNome.textContent = 'nome válido'
         feedbackNome.style.color = 'green'
+        campoNome.style.borderColor = 'green'
         return true
     }
 }
@@ -36,18 +49,21 @@ function validarEmail() {
     if (!valorEmail) {
         feedbackEmail.textContent = 'Insira um email'
         feedbackEmail.style.color = 'red'
+        campoEmail.style.borderColor = 'red'
         return false
     }
 
     else if (!valorEmail.includes('@') || (!valorEmail.includes('.'))) {
         feedbackEmail.textContent = 'email inválido'
         feedbackEmail.style.color = 'red'
+        campoEmail.style.borderColor= 'red'
         return false
     }
 
     else {
         feedbackEmail.textContent = 'email válido'
         feedbackEmail.style.color = 'green'
+        campoEmail.style.borderColor = 'green'
         return true
     }
 }
@@ -58,32 +74,38 @@ function validarSenha() {
     if (!valorSenha) {
         feedbackSenha.textContent = 'Insira uma senha'
         feedbackSenha.style.color = 'red'
+        campoSenha.style.borderColor = 'red'
         return false
     }
 
     else if (valorSenha.length < 6) {
         feedbackSenha.textContent = 'Senha fraca'
         feedbackSenha.style.color = 'red'
+        campoSenha.style.borderColor = 'red'
         return false
     }
 
     else {
         feedbackSenha.textContent = 'Senha válida'
         feedbackSenha.style.color = 'green'
+        campoSenha.style.borderColor = 'green'
         return true
     }
 }
 
 campoSenha.addEventListener('input', function(){
     validarSenha()
+    atualizarBotao()
 })
 
 campoEmail.addEventListener('input', function(){
     validarEmail()
+    atualizarBotao()
 })
 
 campoNome.addEventListener('input', function(){
     validarNome()
+    atualizarBotao()
 })
 
 
